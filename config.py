8fab1e152b87d8a13ea20ea6766118edb61e83f6
sreejith <sreejithc321@gmail.com>
2015-06-17 19:13:29 +0530

    configuration

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-consumer_key = 'NoMTtfHQWLOTFV4Raqmxd6nbS'
-consumer_secret = 'KyCAZQDpD3KtWHCD9Y6XFjNoMbmGty9IlC3QVOGsryXGPibGgW'
-access_token = '3138696066-iUO1U8v4iv643h12YvKX76Z0gY57ag0ADtyMUgG'
-access_token_secret = '1g9ahZvpl6rs51I24d2nqT5j1srlcMs4srKftNxO1UAY9'
+consumer_key = 'enter twitter consumer key'
+consumer_secret = 'enter twitter consumer secret key' 
+access_token = 'enter twitter access token key'
+access_token_secret = 'access twitter token secret key'
 
-TWEET_COUNT = 10000
-SEARCH_KEYWORD = 'Taj Mahal'
+TWEET_COUNT = enter number of tweets to search
+SEARCH_KEYWORD = 'enter keyword to search'
