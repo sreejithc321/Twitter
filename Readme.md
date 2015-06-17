14859cb2bb2eb68129ef23d88c4f7383369f52a2
sreejith <sreejithc321@gmail.com>
2015-06-17 18:47:02 +0530

    Readme file

--- Readme.md
+++ Readme.md
@@ -1,13 +1,13 @@
-# Analyze Twitter Data
-
-Read Tweets and print sentiment.
+# Analyze and Visualize Twitter Data
 
 ### Libraries
 
 - TextBlob : https://github.com/sloria/TextBlob
 - Tweepy : https://github.com/tweepy/tweepy
+- PyLab : http://wiki.scipy.org/PyLab
 
 ### Configuration
 
 - config.py : Store Twitter connection credentials
+              Set Search Keyword and tweet count here
 
--- config.py
+++ config.py
@@ -1,6 +1,7 @@
-consumer_key = '<key>'
-consumer_secret = '<key>'
-access_token = '<key>'
-access_token_secret = '<key>'
+consumer_key = 'NoMTtfHQWLOTFV4Raqmxd6nbS'
+consumer_secret = 'KyCAZQDpD3KtWHCD9Y6XFjNoMbmGty9IlC3QVOGsryXGPibGgW'
+access_token = '3138696066-iUO1U8v4iv643h12YvKX76Z0gY57ag0ADtyMUgG'
+access_token_secret = '1g9ahZvpl6rs51I24d2nqT5j1srlcMs4srKftNxO1UAY9'
 
-
+TWEET_COUNT = 10000
+SEARCH_KEYWORD = 'Taj Mahal'
